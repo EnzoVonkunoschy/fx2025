a0ce340faf48f989ead61994397682dddcf16890
Arali80 <aruss0584@gmail.com>
2025-05-10 00:28:50 -0300

    cambios solicitados

--- src/main/java/com/example/demofx/HelloApplication.java
+++ src/main/java/com/example/demofx/HelloApplication.java
@@ -6,23 +6,56 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.TextArea;
 
-import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+    public void start(Stage stage)  {
 
-        Group controles = new Group();
-        final Button btn = new Button();
-        btn.setText("Hola Mundo fx");
 
-        controles.getChildren().add(btn);
+        final Button btn = new Button("<--->");
+        btn.setPrefWidth(50);
 
-        Scene scene = new Scene(controles, 640, 480);
+        final Button btn2 = new Button("<--->");
+        btn2.setPrefWidth(50);
 
-        stage.setTitle("Hello!");
+        TextArea text_01 = new TextArea("   ");
+        text_01.setPrefSize(200, 30);
+        text_01.setMaxHeight(30);
+        text_01.setMinHeight(30);
+
+        TextArea text_02 = new TextArea("   ");
+        text_02.setPrefSize(200, 30);
+        text_02.setMaxHeight(30);
+        text_02.setMinHeight(30);
+
+        TextArea text_03 = new TextArea("   ");
+        text_03.setPrefSize(200,30 );
+        text_03.setMaxHeight(30);
+        text_03.setMinHeight(30);
+
+        HBox controles = new HBox(10);
+        controles.getChildren().addAll(text_01, btn, text_02, btn2, text_03);
+
+
+        btn.setOnAction(e -> {
+            String temp = text_01.getText();
+            text_01.setText(text_02.getText());
+            text_02.setText(temp);
+        });
+
+        btn2.setOnAction(e -> {
+            String temp = text_02.getText();
+            text_02.setText(text_03.getText());
+            text_03.setText(temp);
+        });
+
+
+        Scene scene = new Scene(controles, 800, 480);
+
+        stage.setTitle("Pasa texto");
         stage.setScene(scene);
         stage.show();
     }
